d823b454f2143c8fc15f23ba747c29f6c2b23eed
spraby <spraby.com@gmail.com>
2025-05-27 21:13:50 +0200

    remove test

--- theme/layouts/ThemeLayout.tsx
+++ theme/layouts/ThemeLayout.tsx
@@ -6,20 +6,6 @@ import LayoutHeader from "@/theme/sections/LayoutHeader";
 export default function ({children, menu}: { children: ReactNode, menu: any }) {
   return <div>
     <LayoutHeader menu={menu}/>
-    <div className={'flex gap-5'}>
-      <div  className={'bg-yellow-200 rounded-3xl p-5'} onClick={() => {
-        window.location.href = `tiktok://share?url=${encodeURIComponent('https://google.com')}`;
-        setTimeout(() => {
-          window.open(`https://www.tiktok.com/share?url=${encodeURIComponent('https://google.com')}`, '_blank');
-        }, 1000);
-      }} >Tiktok</div>
-      <div  className={'bg-yellow-400 rounded-3xl p-5'}  onClick={() => {
-        navigator?.share({url: 'https://google.com'}).finally();
-      }}>Instagram</div>
-      <div  className={'bg-yellow-700 rounded-3xl p-5'} onClick={() => {
-        window.location.href = `fb://facewebmodal/f?href=${encodeURIComponent('https://google.com')}`;
-      }} >fb</div>
-    </div>
     {children}
   </div>
 }
