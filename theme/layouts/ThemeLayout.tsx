7b7435e7eba75cd60cbf162eba603a0e78f632a2
spraby <spraby.com@gmail.com>
2025-05-27 21:05:18 +0200

    test

--- theme/layouts/ThemeLayout.tsx
+++ theme/layouts/ThemeLayout.tsx
@@ -6,20 +6,20 @@ import LayoutHeader from "@/theme/sections/LayoutHeader";
 export default function ({children, menu}: { children: ReactNode, menu: any }) {
   return <div>
     <LayoutHeader menu={menu}/>
-    <div className={'bg-yellow-200 rounded-3xl p-5'} onClick={() => {
-
-      if (navigator.share) {
-        navigator.share({
-          title: 'Смотри, что я нашёл',
-          text: 'Полезная ссылка',
-          url: 'https://google.com'
-        })
-          .then(() => console.log('Успешно расшарено'))
-          .catch(err => console.error('Ошибка шаринга', err));
-      } else {
-        alert('Шаринг не поддерживается в этом браузере');
-      }
-    }}> test </div>
+    <div className={'flex gap-5'}>
+      <div  className={'bg-yellow-200 rounded-3xl p-5'} onClick={() => {
+        window.location.href = `tiktok://share?url=${encodeURIComponent('https://google.com')}`;
+        setTimeout(() => {
+          window.open(`https://www.tiktok.com/share?url=${encodeURIComponent('https://google.com')}`, '_blank');
+        }, 1000);
+      }} >Tiktok</div>
+      <div  className={'bg-yellow-400 rounded-3xl p-5'}  onClick={() => {
+        navigator?.share({url: 'https://google.com'}).finally();
+      }}>Instagram</div>
+      <div  className={'bg-yellow-700 rounded-3xl p-5'} onClick={() => {
+        window.location.href = `fb://facewebmodal/f?href=${encodeURIComponent('https://google.com')}`;
+      }} >fb</div>
+    </div>
     {children}
   </div>
 }
